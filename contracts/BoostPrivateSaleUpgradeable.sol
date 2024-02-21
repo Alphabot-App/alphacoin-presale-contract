1dd3abf53e1078e01d5a4400c2d4ddf649bb1ef0
Billy <cryptobilly37@gmail.com>
2024-02-21 12:58:04 +0400

    Update contract

--- contracts/BoostPrivateSaleUpgradeable.sol
+++ contracts/BoostPrivateSaleUpgradeable.sol
@@ -6,44 +6,60 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract BoostPrivateSaleUpgradeable is Initializable, OwnableUpgradeable {
-    IERC20 public usdt;
-    IERC20 public usdc;
+    IERC20 internal usdt;
+    IERC20 internal usdc;
+    address internal out_addr;
 
-    event PrivateSale(address indexed sender, address indexed token, uint256 value);
-    event Withdrawn(address indexed token, uint256 value, address indexed recipient);
+    event PrivateSale(
+        address indexed sender,
+        address indexed token,
+        uint256 value
+    );
+    event Withdrawn(
+        address indexed token,
+        uint256 value,
+        address indexed recipient
+    );
 
     function initialize(
         IERC20 _usdt,
-        IERC20 _usdc
+        IERC20 _usdc,
+        address _out_addr
     ) public initializer {
         usdt = _usdt;
         usdc = _usdc;
-        __Ownable_init(
-            msg.sender
-        );
+        out_addr = _out_addr;
+
+        __Ownable_init(msg.sender);
     }
 
     function privateSaleWithEth() external payable {
         require(msg.value > 0, "Non zero value");
+        payable(out_addr).transfer(msg.value);
         emit PrivateSale(msg.sender, address(0x0), msg.value);
     }
 
-    function privateSaleWithToken(
-        address token,
-        uint256 amount
-    ) external {
-        require(token == address(usdt) || token == address(usdc), "BoostPrivateSaleUpgradeable: Invalid token");
+    function privateSaleWithToken(address token, uint256 amount) external {
+        require(
+            token == address(usdt) || token == address(usdc),
+            "BoostPrivateSaleUpgradeable: Invalid token"
+        );
         require(amount > 0, "BoostPrivateSaleUpgradeable: Non zero value");
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        IERC20(token).transferFrom(msg.sender, out_addr, amount);
         emit PrivateSale(msg.sender, token, amount);
     }
 
+    function setOutAddress(address new_addr) external onlyOwner {
+        out_addr = new_addr;
+    }
+
+    // Keeping withdraw function in case for some reason we get some kind of eth or token sent directly to the contract
     function withdraw(
         address token,
         uint256 amount,
-        address recipient        
+        address recipient
     ) external onlyOwner {
-        if(token == address(0x0)) {
+        if (token == address(0x0)) {
             payable(recipient).transfer(amount);
         } else {
             IERC20(token).transfer(recipient, amount);
