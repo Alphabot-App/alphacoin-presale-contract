1a1904c127c8e844dac75af76f1293c6b2721352
Billy <cryptobilly37@gmail.com>
2024-05-29 09:49:00 -0600

    Add hardhat verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,10 +3,16 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "tsconfig-paths/register";
+import "@nomicfoundation/hardhat-verify";
 
 config();
 
-const { API_URL, PRIVATE_KEY, NETWORK = 'hardhat' } = process.env;
+const {
+  API_URL,
+  PRIVATE_KEY,
+  NETWORK = "hardhat",
+  ETHERSCAN_API_KEY,
+} = process.env;
 
 const configObj: HardhatUserConfig = {
   solidity: "0.8.20",
@@ -17,6 +23,16 @@ const configObj: HardhatUserConfig = {
       accounts: [`0x${PRIVATE_KEY}`],
     },
     hardhat: {},
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true,
   },
 };
 
